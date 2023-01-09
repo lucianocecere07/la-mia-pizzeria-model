b62750e1595a5991c586140e60a36e2fc224c35f
lucianocecere07 <cecereluciano97@gmail.com>
2023-01-09 16:51:34 +0100

    Modificato path delle immagini

--- LaMiaPizzeriaModel/Utils/PizzeLista.cs
+++ LaMiaPizzeriaModel/Utils/PizzeLista.cs
@@ -15,12 +15,12 @@ namespace LaMiaPizzeriaModel.Utils
 
             pizze = new List<Pizza>();
 
-            Pizza pizza1 = new Pizza(1, "Margherita", "pomodoro, mozzarella e basilico", "https://www.google.it/url?sa=i&url=https%3A%2F%2Fwww.cookist.it%2Fpizza-margherita-fatta-in-casa%2F&psig=AOvVaw21ThIIdiotEtlolktUA3GQ&ust=1673359993222000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCKDXndbVuvwCFQAAAAAdAAAAABAS", 5.50);
-            Pizza pizza2 = new Pizza(2, "Marinara", "pomodoro, aglio, origano e olio", "C:\\File Luciano\\Studi\\dotnet generation\\esercizi GIT\\LaMiaPizzeriaModel\\LaMiaPizzeriaModel\\wwwroot\\img\\pizza-marinara.jpg", 5.00);
-            Pizza pizza3 = new Pizza(3, "Diavola", "pomodoro, mozzarella, basilico e salame piccante", "~/img/pizza-diavola.jpg", 6.50);
-            Pizza pizza4 = new Pizza(4, "Napoli", "pomodoro, mozzarella e alici", "~/img/pizza-napoli.jpg", 6.50);
-            Pizza pizza5 = new Pizza(5, "Crostino", "mozzarella e prosciutto cotto", "~/img/pizza-crostino.jpg", 6.00);
-            Pizza pizza6 = new Pizza(6, "Ortolana", "mozzarella, zucchine, peperoni e funghi", "~/img/pizza-ortolana.jpg", 7.00);
+            Pizza pizza1 = new Pizza(1, "Margherita", "pomodoro, mozzarella e basilico", "/img/pizza-margherita.jpg", 5.50);
+            Pizza pizza2 = new Pizza(2, "Marinara", "pomodoro, aglio, origano e olio", "/img/pizza-marinara.jpg", 5.00);
+            Pizza pizza3 = new Pizza(3, "Diavola", "pomodoro, mozzarella, basilico e salame piccante", "/img/pizza-diavola.jpg", 6.50);
+            Pizza pizza4 = new Pizza(4, "Napoli", "pomodoro, mozzarella e alici", "/img/pizza-napoli.jpg", 6.50);
+            Pizza pizza5 = new Pizza(5, "Crostino", "mozzarella e prosciutto cotto", "/img/pizza-crostino.jpg", 6.00);
+            Pizza pizza6 = new Pizza(6, "Ortolana", "mozzarella, zucchine, peperoni e funghi", "/img/pizza-ortolana.jpg", 7.00);
 
             pizze.Add(pizza1);
             pizze.Add(pizza2);
@@ -32,4 +32,5 @@ namespace LaMiaPizzeriaModel.Utils
             return pizze;
         }
     }
+    
 }
